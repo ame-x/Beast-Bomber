5e1bc5e3a64fa7e3f774b2968cc5c1d9cd96d4ca
Rucy <127585375+rucykun@users.noreply.github.com>
2024-03-29 16:18:01 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-var input_chanelID = addInput("Channel ID");
-var input_saying = addTextarea("Message");
+var channelid-input = addInput("Channel ID");
+var content-input = addTextarea("Message");
 h.append("<br>");
-var btn_start = addBtn("Send");
-var btn_stop = addBtn("Stop").hide();
+var send-btn = addBtn("Send");
+var stop-btn = addBtn("Stop").hide();
 var nowStatus = $("<div>").appendTo(h);
 h.append("<br><br><br><br>");
