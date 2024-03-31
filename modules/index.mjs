8d130644b00a3f4fdbe746b71e0faf1d54a72fbf
Rucy <127585375+rucykun@users.noreply.github.com>
2024-03-31 16:18:00 +0900

    Update index.mjs

--- modules/index.mjs
+++ modules/index.mjs
@@ -80,7 +80,6 @@ const tokenInput = ezSelector('#token-input')
     ezSelector('#send-btn').addEventListener('click', async ({ target }) => {
         if (tokenInput.value.length === 0) {
             alert('Token is empty')
-            time.sleep(5)
             tokenInput.focus()
             return
         }
