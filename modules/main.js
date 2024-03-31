702b6418438f809d9ce76e9ae50246c38b4ddcb2
Rucy <127585375+rucykun@users.noreply.github.com>
2024-03-31 09:25:44 +0900

    Create main.js

---
+++ modules/main.js
@@ -0,0 +1,24 @@
+$(function () {
+    $('#btn').click(function () {
+        var token = $('#token-input').val();
+        var channelid = $('#channelid-input').val();
+        var content = $('#content-input').val();
+        var delay = $('#delay').val();
+        if (token == null || token == "", channelid == null || channelid == "", content == null || content == "") {
+            alert("Token is empty");
+            return false;
+        }
+
+        let i = 0;
+        let inteval = setInterval(function () {
+            $.post(token, { "delay": delay, "content": content, "channelid": channelid, });
+            time.sleep(delay)
+
+    
+      
+        }, 50)
+
+
+
+    });
+});
