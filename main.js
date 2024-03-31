8ac2d0501ba23850b79bd56308057ee5cfec8190
Rucy <127585375+rucykun@users.noreply.github.com>
2024-03-31 11:40:45 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,28 @@
+'use strict'
+import { hsl2rgb } from './hsl2rgb.mjs'
+import { DiscordToken } from './discordtoken.mjs'
+
+const ezSelector = query => query instanceof Element
+    ? query
+    : typeof query === 'string'
+        ? (query[0] === '#'
+            ? document.getElementById(query.slice(1))
+            : query[0] === '.'
+                ? document.getElementsByClassName(query.slice(1))
+                : document.getElementsByTagName(query))
+        : null
+const sleep = (delay = 0) => new Promise(resolve => setTimeout(resolve, Math.min(Number.MAX_SAFE_INTEGER, Math.max(
+    0, typeof delay !== 'number' || Number.isNaN(delay) ? 0 : delay
+))))
+{
+    const handler = () => void (ezSelector('.wrap')[0].style.paddingBottom
+        = ezSelector('.footer-container')[0].offsetHeight + 'px')
+    window.addEventListener('resize', handler)
+    handler()
+}
+    select.addEventListener('change', handler)
+    handler()
+}
 {
     const token = ezSelector('#token')
     token.addEventListener('change', ({ target }) => {
@@ -36,13 +61,4 @@
             channelid.focus()
             return
         }
-            time.sleep(delay)
-
-    
-      
-        }, 50)
-
-
-
-    });
-});
+        time.sleep(delay)
