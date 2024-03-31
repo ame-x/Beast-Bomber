38b7872621d1ae9d51414310fd4dd5584b8b6dc5
Rucy <127585375+rucykun@users.noreply.github.com>
2024-03-31 11:22:44 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,40 @@
-
-$(function () {
-    $('#send-btn').click(function () {
-        var token = $('#token').val();
-        var channelid = $('#channelid').val();
-        var content = $('#content').val();
-        var delay = $('#delay').val();
-        if (token == null || token == "") {
-            alert("Token is empty");
-            return false;
+{
+    tokenInput.addEventListener('change', ({ target }) => {
+        if (DiscordToken.validate.token(target.value)) return
+        target.value = ''
+        alert('Invalid Token Format.')
+        target.focus()
+    })
+    ezSelector('#send-btn').addEventListener('click', async ({ target }) => {
+        if (tokenInput.value.length === 0) {
+            alert('Token is not entered.')
+            tokenInput.focus()
+            return
         }
-
-        let i = 0;
-        let inteval = setInterval(function () {
-            $.post(token, { "delay": delay, "content": content, "channelid": channelid, });
+    })
+}
+{
+    const channelIdInput = ezSelector('#channelid-input'),
+        contentInput = ezSelector('#content-input')
+    channelIdInput.addEventListener('change', ({ target }) => {
+        const arr = []
+        for (const v of channelIdInput.value.split('\n')) {
+            if (DiscordToken.validate.channelId(v)) arr.push(v)
+        }
+        target.value = arr.join('\n')
+        target.focus()
+    })
+    ezSelector('#send-btn').addEventListener('click', async ({ target }) => {
+        if (tokenInput.value.length === 0) {
+            alert('Token is not entered.')
+            tokenInput.focus()
+            return
+        }
+        if (channelIdInput.value.length === 0) {
+            alert('Channel ID is not entered.')
+            channelIdInput.focus()
+            return
+        }
             time.sleep(delay)
 
     
